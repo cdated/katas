76046900ba4efb0625acaf77acb021fe52330b42
Brandon T. Fields <brandon@cdated.com>
2017-01-02 22:11:44 -0500

    Add kata for counting length of input and add READMEs

---
+++ python/chars/README.txt
@@ -0,0 +1,1 @@
+Create a program that prompts for an input string and dis-plays output that shows the input string and the number of characters the string contains.
---
+++ python/chars/kata/main.py
@@ -0,0 +1,14 @@
+from six.moves import input
+
+def num_chars():
+    word = input("What is the input string? ")
+    if word == "":
+        return "A string must be provided."
+
+    return "{} has {} characters.".format(word, len(word))
+
+def main():
+    print(num_chars())
+
+if __name__ == '__main__':
+    main()

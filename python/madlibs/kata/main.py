8f4999b551e316c956efe00fad943fe07cd8eea6
Brandon T. Fields <brandon@cdated.com>
2017-02-03 21:12:04 -0500

    Complete madlibs kata

--- python/madlibs/kata/main.py
+++ python/madlibs/kata/main.py
@@ -2,10 +2,27 @@ from __future__ import print_function
 from six.moves import input
 
 
-def main():
-    data = input('')
+TEMPLATE = """A boy and his mother went to the store to buy a \
+{noun} they needed one that was {adjective}. However, they would have \
+to {verb} {adverb}."""
 
-    return data
+
+def get_answers():
+    """ Get users responses as dict """
+
+    values = dict()
+    values["noun"] = input('Enter a noun: ')
+    values["verb"] = input('Enter a verb: ')
+    values["adjective"] = input('Enter a adjective: ')
+    values["adverb"] = input('Enter a adverb: ')
+
+    return values
+
+
+def main():
+
+    response = TEMPLATE.format(**get_answers())
+    print(response)
 
 
 if __name__ == '__main__':
--- python/madlibs/kata/tests/test_main.py
+++ python/madlibs/kata/tests/test_main.py
@@ -1,8 +1,28 @@
 from kata import main
-from mock import patch, call
 from unittest import TestCase
+import mock
+
 
 class Test(TestCase):
-    def test_read_input(self):
-        with patch('kata.main.input', return_value='valid input'):
-            self.assertEqual(main.main(), 'valid input')
+
+    def test_template(self):
+        tpl = main.TEMPLATE
+        self.assertTrue(isinstance(tpl, str))
+
+    def test_get_answers(self):
+        expected = dict(noun='a', verb='b', adjective='c', adverb='d')
+        answers = ['a', 'b', 'c', 'd']
+
+        main.input = mock.Mock(side_effect=answers)
+        values = main.get_answers()
+        self.assertEqual(values, expected)
+
+    @mock.patch('kata.main.print')
+    def test_main(self, mock_print):
+        main.TEMPLATE = template = "{vala} + {valb}"
+        values = dict(vala='a', valb='b')
+
+        expected = template.format(**values)
+        main.get_answers = mock.Mock(return_value=values)
+        main.main()
+        self.assertEqual(mock_print.mock_calls, [mock.call(expected)])

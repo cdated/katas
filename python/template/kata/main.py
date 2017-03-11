1b60c13caa7105dd15ae07b57b5c02da6f8155f7
Brandon T. Fields <brandon@cdated.com>
2017-03-11 12:30:22 -0500

    Add a print test case to template
    Import mock and use mock.* pattern

--- python/template/kata/main.py
+++ python/template/kata/main.py
@@ -2,10 +2,13 @@ from __future__ import print_function
 from six.moves import input
 
 
-def main():
+def get_input():
     data = input('')
-
     return data
+
+
+def main():
+    print(get_input())
 
 
 if __name__ == '__main__':
--- python/template/kata/tests/test_main.py
+++ python/template/kata/tests/test_main.py
@@ -1,8 +1,19 @@
 from kata import main
-from mock import patch, call
+import mock
 from unittest import TestCase
 
+
 class Test(TestCase):
+
     def test_read_input(self):
-        with patch('kata.main.input', return_value='valid input'):
-            self.assertEqual(main.main(), 'valid input')
+        with mock.patch('kata.main.input', return_value='valid input'):
+            self.assertEqual(main.get_input(), 'valid input')
+
+    @mock.patch('kata.main.print')
+    @mock.patch('kata.main.get_input', return_value="Good day!")
+    def test_main_print(self, mock_get_input, mock_print):
+        greeting = "Good day!"
+        main.main()
+
+        calls = [mock.call(greeting)]
+        self.assertEqual(mock_print.mock_calls, calls)
